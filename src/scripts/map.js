09ea12e7020d5fe244df1bf4e3091d5c47b711b3
Dreamacro <305009791@qq.com>
2015-08-28 20:07:16 +0800

    fireball 特效

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -6,13 +6,33 @@ class Map {
         this.scale = this.WIDTH / 1000;
 
         this.canvas = document.createElement('canvas');
-        option.container.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext('2d');
         this.canvas.width = this.WIDTH;
         this.canvas.height = this.HEIGHT;
-        this.ctx = this.canvas.getContext('2d');
+        option.container.appendChild(this.canvas);
+
         this.ctx.drawSvg('world.svg', 0, 0, this.WIDTH, this.HEIGHT);
-        this.mapDate = this.ctx.getImageData(0,0,this.WIDTH,this.HEIGHT);
+        this.mapDate = this.ctx.getImageData(0, 0, this.WIDTH,this.HEIGHT);
         this.container = {};
+
+        this.particles = {}
+        this.particle = (() => {
+          let nextIndex = 0;
+          return (x,y,r,o,c,xv,yv,rv,ov) => {
+            this.particles[++nextIndex] = {
+              index: nextIndex,
+              x: x,
+              y: y,
+              r: r,
+              o: o,
+              c: c,
+              xv: xv,
+              yv: yv,
+              rv: rv,
+              ov: ov
+            };
+          };
+        })();
         this.count = 0;// queue总数
     }
 
@@ -25,18 +45,22 @@ class Map {
     trigger(start, end) {
         start = this.getXY(start);
         end = this.getXY(end);
-        const timestamp = +new Date;
-        const startColor = '#21b384';
-        const endColor = '#e32528';
+        const timestamp = +new Date,
+              startColor = '#21b384',
+              endColor = '#e32528',
+              particleColor = '#F8BD19';
         if(this.container[timestamp]) return;// 无奈
         let info = {
             start: start,
             end: end,
             startColor: startColor,
             endColor: endColor,
+            particleColor: particleColor,
             duration: 1000,
             radius: 70 + parseInt(Math.random() * 20),
-            stage: 0
+            stage: 0,
+            xv: (start.x - end.x) / 500,
+            yv: (start.y - end.y) / 500
         }
         this.container[timestamp] = info;
         if(!(this.count)++) {
@@ -56,13 +80,30 @@ class Map {
     draw() {
         const now = +new Date;
         this.clear();
+
+        // particles
+        for(let key of Object.keys(this.particles)) {
+            let p = this.particles[key];
+            this.ctx.beginPath();
+            this.ctx.arc(p.x,p.y,p.r,0,Math.PI*2);
+            this.ctx.globalAlpha = p.o;
+            this.ctx.fillStyle = p.c;
+            this.ctx.fill();
+            p.x += p.xv;
+            p.y += p.yv;
+            p.r += p.rv;
+            p.o += p.ov;
+            if(p.r < 0 || p.o < 0) delete this.particles[p.index];
+        }
+
+        // fireballs
         for(let key of Object.keys(this.container)) {
             const time = now - key;
             const info = this.container[key];
             if(time < info.duration) {
                 const r = this.easeOutQuint(time, 0, info.radius, info.duration);
-                const offsetx = this.easeOutQuint(time > info.duration / 2 ? time - info.duration / 2 : time , info.start.x, info.end.x - info.start.x, info.duration / 2);
-                const offsety = this.easeOutQuint(time > info.duration / 2 ? time - info.duration / 2 : time, info.start.y, info.end.y - info.start.y, info.duration / 2);
+                const offsetx = info.start.x + time / info.duration * (info.end.x - info.start.x); //this.easeOutQuint(time, info.start.x, info.end.x - info.start.x, info.duration);
+                const offsety = info.start.y + time / info.duration * (info.end.y - info.start.y);//this.easeOutQuint(time, info.start.y, info.end.y - info.start.y, info.duration);
                 const opacity = this.easeOutQuint(time, 1, -1, info.duration);
 
                 // circle
@@ -73,18 +114,25 @@ class Map {
                 this.ctx.arc(info.start.x, info.start.y, r, 0, 2*Math.PI);
                 this.ctx.stroke();
 
-                // line
-                this.ctx.globalAlpha = 0.5;
-                this.ctx.strokeStyle = info.endColor;
-                this.ctx.beginPath();
-                time < info.duration / 2 ? this.ctx.moveTo(info.start.x, info.start.y) : this.ctx.moveTo(info.end.x, info.end.y);
-                //this.ctx.moveTo(info.start.x, info.start.y);
-                this.ctx.lineTo(offsetx, offsety);
-                this.ctx.stroke();
+                let numParticles = Math.sqrt(info.xv*info.xv+info.yv*info.yv)/5;
+                if(numParticles<1)numParticles=1;
+                let numParticlesInt = Math.ceil(numParticles),
+                    numParticlesDif = numParticles/numParticlesInt;
+                for(let j=0;j<numParticlesInt;j++) {
+                  this.particle(
+                    offsetx-info.xv*j/numParticlesInt,
+                    offsety-info.yv*j/numParticlesInt,
+                    7,
+                    numParticlesDif,
+                    info.particleColor,
+                    Math.random()*0.6,
+                    Math.random()*0.6,
+                    -0.3,
+                    -0.05*numParticlesDif
+                  );
+                }
             } else if(time < info.duration * 2) {
                 const r = this.easeOutQuint(time - info.duration, 0, info.radius, info.duration);
-                const offsetx = this.easeOutQuint(time - info.duration, info.start.x, info.end.x - info.start.x, info.duration);
-                const offsety = this.easeOutQuint(time - info.duration, info.start.y, info.end.y - info.start.y, info.duration);
                 const opacity = this.easeOutQuint(time - info.duration, 1, -1, info.duration);
 
                 // circle
@@ -94,14 +142,6 @@ class Map {
                 this.ctx.beginPath();
                 this.ctx.arc(info.end.x, info.end.y, r, 0, 2*Math.PI);
                 this.ctx.stroke();
-
-                // // line
-                // this.ctx.globalAlpha = 0.5;
-                // this.ctx.strokeStyle = info.endColor;
-                // this.ctx.beginPath();
-                // this.ctx.moveTo(info.end.x, info.end.y);
-                // this.ctx.lineTo(offsetx, offsety);
-                // this.ctx.stroke();
             } else {
                 delete this.container[key];
                 this.count--;
@@ -113,7 +153,7 @@ class Map {
     }
 
     /**
-     * 经纬度2XY
+     * 经纬度 to XY
      * @param  {Object} option
      * @return {Object}
      */
